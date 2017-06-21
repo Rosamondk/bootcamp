a5a349c18af64d436eebe10b2093c0a490af532c
admin <admin@localhost.localdomain>
2017-06-20 18:26:05 -0700

    first commit comment

---
+++ hello/hello.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+int main()
+{
+  if(3<2) {
+    printf("True\n");
+  } else {
+    printf("F\n");
+  }
+
+  if(3-(2+1)) {
+    printf("True\n");
+  } else {
+    printf("F\n");
+  }
+  if('d') {
+    printf("True\n");
+  } else {
+    printf("F\n");
+  }
+
+  if(9%3) {
+  printf("True\n");
+} else {
+  printf("F\n");
+}
+if((3>2)-1) {
+  printf("True\n");
+} else {
+  printf("F\n");
+}
+return 0;
+
+}
